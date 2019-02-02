3e7252b98dc93a317ddd2448c0f9613d4b8f2ce5
Paul Attara <paul.attara@mail.mcgill.ca>
2019-02-02 17:09:52 -0500

    changed firebase credentials

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,13 +2,13 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+var config = {
+  apiKey: "AIzaSyB_77KFpFlWRUhr6bbFNsZ51GqoGngt44k",
+  authDomain: "spafb-d911f.firebaseapp.com",
+  databaseURL: "https://spafb-d911f.firebaseio.com",
+  projectId: "spafb-d911f",
+  storageBucket: "spafb-d911f.appspot.com",
+  messagingSenderId: "1089758683190"
 };
 
 class Firebase {
